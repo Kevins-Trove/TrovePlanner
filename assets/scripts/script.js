306e8f7ba1ed9d0c96045366e013a15f0b496078
Kevin Roper <kevin@kevinstrove.com>
2024-03-11 20:01:47 -0700

    Created data storage logic.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,13 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Global varibles
+var data;
+var localName = "TrovePlanner";
+var min = 11;
+var max = 22;
+
 $(function () {
+  updateClock();
+  getData();
+  updatePlanner();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -26,23 +32,22 @@ setInterval(() => {
   updateClock();
 }, 1000);
 
-updateClock()
+
 function updateClock() {
   $('#currentDay').text(dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'));
 }
 
-updatePlanner();
+
 function updatePlanner(){
-  var min = 11;
-  var max = 22;
   var currentHour = dayjs().hour();
 
     // Generate hour slots
     for (let i = min; i <= max; i++) { 
+      // Hour div
       var hour = $('<div>');
-      // Set the class of the div
       hour.addClass("hour row time-block");
-      
+ 
+      // Set class by time slot
       if (i < currentHour  ){
         hour.addClass("past");
       } else if (i == currentHour) {
@@ -50,10 +55,10 @@ function updatePlanner(){
       } else {
         hour.addClass("future");
       }
-
       
       hour.attr("id", "hour-" + i);
       
+      // Create coloum for text area
       var col = $('<div>');
       col.addClass("col-2 col-md-1 hour text-center py-3");
       
@@ -61,15 +66,29 @@ function updatePlanner(){
       hour.append(col);
 
       var textArea = $('<textarea>');
+      textArea.attr("id", "text" + i);
       textArea.addClass("col-8 col-md-10 description");
       textArea.attr("rows", "3" );
       textArea.attr("aria-label", "save" );
+      
+      var matchingItem = data.filter(function(item) {
+        return item.hour === i;
+      });
+      textArea.val(matchingItem.text);
+      
       hour.append(textArea);
 
+      // Creat button 
       var button = $('<button>');
+      button.attr("id", "button" + i);
       button.addClass("btn saveBtn col-2 col-md-1");
       button.attr("aria-label", "save" );
+      // Assing click event to button
       
+      button.click(function() {
+        var buttonId = $(this).attr("id");
+        saveData(buttonId);
+    });
 
       var iClass = $('<i>');
       iClass.addClass("fas fa-save");
@@ -77,10 +96,42 @@ function updatePlanner(){
       button.append(iClass);
       
       hour.append(button);
-     
       
       $('#calendar').append(hour);
 
     }
 }
 
+// Function to save button click by index
+function saveData(buttonId){
+  var matchingItem = data.filter(function(item) {
+    return item.hour === i;
+  });
+  textArea.val(matchingItem.text);
+  alert(buttonId);
+}
+
+function getData(){
+  var result;
+
+  result = localStorage.getItem(localName);
+  
+  if (result) {
+    data = JSON.parse(result);
+  } else {
+    // Create blank array if it does not exist
+     // Generate hour slots
+     var slot = {
+      hour: 0,
+      text: ""
+     }
+
+     data = [];
+     for (let i = min; i <= max; i++) { 
+      slot.hour = i;
+      slot.text = ""
+      data.push(slot);
+    }
+  }
+
+}
