a4e897a21dcb3dd57efd66f26f9360e690cc4fc4
Kevin Roper <kevin@kevinstrove.com>
2024-03-11 19:16:47 -0700

    Fixed button logic error.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -33,20 +33,31 @@ function updateClock() {
 
 updatePlanner();
 function updatePlanner(){
-  var min = 8;
-  var max = 18;
+  var min = 11;
+  var max = 22;
+  var currentHour = dayjs().hour();
 
-  
     // Generate hour slots
     for (let i = min; i <= max; i++) { 
       var hour = $('<div>');
       // Set the class of the div
-      hour.addClass("hour row time-block present");
+      hour.addClass("hour row time-block");
+      
+      if (i < currentHour  ){
+        hour.addClass("past");
+      } else if (i == currentHour) {
+        hour.addClass("present");
+      } else {
+        hour.addClass("future");
+      }
+
+      
       hour.attr("id", "hour-" + i);
       
       var col = $('<div>');
       col.addClass("col-2 col-md-1 hour text-center py-3");
-      col.text(i+ "AM");
+      
+      col.text( dayjs().hour(i).format('hh A'));
       hour.append(col);
 
       var textArea = $('<textarea>');
@@ -58,18 +69,15 @@ function updatePlanner(){
       var button = $('<button>');
       button.addClass("btn saveBtn col-2 col-md-1");
       button.attr("aria-label", "save" );
-      hour.append(button);
+      
 
       var iClass = $('<i>');
       iClass.addClass("fas fa-save");
       iClass.attr("aria-hidden", "true" );
-      hour.append(button);
-//<i class="fas fa-save" aria-hidden="true"></i>
-      hour.append(button);
-
+      button.append(iClass);
       
-      console.log(hour);
-      
+      hour.append(button);
+     
       
       $('#calendar').append(hour);
 
